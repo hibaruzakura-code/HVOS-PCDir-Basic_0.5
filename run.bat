cf0e60c9ec8c74acc90f2a5d94f76287a14db25f
kazenikike <ranpaku@gmail.com>
2026-09-17 17:57:13 +0900

    Update run.bat

--- run.bat
+++ run.bat
@@ -2,10 +2,10 @@
 chcp 932 >nul
 
 python app_main_base_pc_dir.py
-if %errorlevel% neq 0 (
+if %ERRORLEVEL% neq 0 (
     echo.
-    echo 【エラー】起動に失敗しました。
-    echo setup.bat を実行済みか、ファイル名が正しいか確認してください。
+    echo [ERROR] Failed to start.
+    echo Please check setup.bat or file name.
     echo.
     pause
 )
